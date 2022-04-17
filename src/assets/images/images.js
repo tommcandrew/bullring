6aca1c83eae99b5eaeb0ac06e971f17534442c15
Tom McAndrew <tom.mcandrew@appscore.net>
2022-04-17 19:03:32 +0100

    add some fashion images to assets folder as images from mock api not working

---
+++ src/assets/images/images.js
@@ -0,0 +1,43 @@
+import fashion1 from "./fashion1.jpeg";
+import fashion2 from "./fashion2.jpeg";
+import fashion3 from "./fashion3.jpeg";
+import fashion4 from "./fashion4.jpeg";
+import fashion5 from "./fashion5.jpeg";
+import fashion6 from "./fashion6.jpeg";
+import fashion7 from "./fashion7.jpeg";
+import fashion8 from "./fashion8.jpeg";
+import fashion9 from "./fashion9.jpeg";
+import fashion10 from "./fashion10.jpeg";
+import fashion11 from "./fashion11.jpeg";
+import fashion12 from "./fashion12.jpeg";
+import fashion13 from "./fashion13.jpeg";
+import fashion14 from "./fashion14.jpeg";
+import fashion15 from "./fashion15.jpeg";
+import fashion16 from "./fashion16.jpeg";
+import fashion17 from "./fashion17.jpeg";
+import fashion18 from "./fashion18.jpeg";
+import fashion19 from "./fashion19.jpeg";
+import fashion20 from "./fashion20.jpeg";
+
+export default [
+  fashion1,
+  fashion2,
+  fashion3,
+  fashion4,
+  fashion5,
+  fashion6,
+  fashion7,
+  fashion8,
+  fashion9,
+  fashion10,
+  fashion11,
+  fashion12,
+  fashion13,
+  fashion14,
+  fashion15,
+  fashion16,
+  fashion17,
+  fashion18,
+  fashion19,
+  fashion20,
+];
