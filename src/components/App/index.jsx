e5e0b9ee01de0ba8cec0e64a822708adae5c72cf
Tom McAndrew <tom.mcandrew@appscore.net>
2022-04-17 19:08:09 +0100

    map over posts and display service name, text and a local image for each

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import fashionImages from "../../assets/images/images";
 
 function App() {
-  fetch('http://private-cc77e-aff.apiary-mock.com/posts')
-    .then((response) => response.json())
-    .then((data) => console.log(data));
-  return <div>Hello world</div>;
-}
+  const [posts, setPosts] = useState([]);
 
+  async function fetchData() {
+    const res = await axios.get("http://private-cc77e-aff.apiary-mock.com/posts");
+    setPosts(res.data.items);
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    console.log("posts :>> ", posts);
+  }, [posts]);
+
+  return (
+    <div>
+      {posts.map((post, i) => (
+        <div key={post.item_id}>
+          <p>{post.service_name}</p>
+          <img width="100px" src={fashionImages[i]} alt="#" />
+          <p>{post.item_data.text}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
 export default App;
