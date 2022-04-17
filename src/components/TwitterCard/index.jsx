2349209e5a49c8c31a12fd7665639733abbd9320
Tom McAndrew <tom.mcandrew@appscore.net>
2022-04-17 22:01:39 +0100

    render username and tweet content in TwitterCard, using twitter-text package to parse links

--- src/components/TwitterCard/index.jsx
+++ src/components/TwitterCard/index.jsx
@@ -1,8 +1,18 @@
 import React from "react";
+import twitter from "twitter-text";
+import { CardWrapper, CardUsername, CardText } from "../../styles/card";
 
-function TwitterCard() {
+function TwitterCard({ post, img }) {
+  const res = twitter.autoLink(twitter.htmlEscape(post.item_data.tweet));
+  const innerHtml = { __html: res };
+
   return (
-    <div></div>
+    <CardWrapper>
+      <CardUsername>
+        {post.item_data.user.username}
+      </CardUsername>
+      <CardText dangerouslySetInnerHTML={innerHtml} />
+    </CardWrapper>
   );
 }
 
