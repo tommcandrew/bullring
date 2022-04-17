167f813428e215b2dd287769756dfd4945e66a56
Tom McAndrew <tom.mcandrew@appscore.net>
2022-04-17 22:34:43 +0100

    update card styles

--- src/styles/card.js
+++ src/styles/card.js
@@ -1,28 +1,36 @@
 import styled from "styled-components";
+import colors from "./colors";
 
 export const CardWrapper = styled.div`
-    margin: 0 1rem 5rem 1rem;
-    border: 1px solid rgba(0,0,0,0.2);
-    border-radius: 6px;
-    padding: 0.5rem;
+  margin: 0 1rem 5rem 1rem;
+  border: 1px solid rgba(0, 0, 0, 0.2);
+  border-radius: 6px;
+  padding: 0.5rem;
 `;
 
 export const CardText = styled.p`
-margin: 1rem 0;
+  margin: 1rem 0;
+  text-align: center;
 `;
 
 export const CardImageWrapper = styled.div`
-    width: 100%;
-    `;
+  width: 100%;
+  margin-bottom: 1rem;
+`;
 
 export const CardLinkWrapper = styled.div`
-    display: flex;
-    justify-content: center;
-    `;
+    text-align: center;
+`;
 
 export const CardContent = styled.div`
-    display: flex;
-    justify-content: center;
-    flex-direction: column;
-    padding: 1rem;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  padding: 1rem;
+`;
+
+export const CardUsername = styled.div`
+  font-weight: bold;
+  text-align: center;
+  color: ${colors.heading};
 `;
