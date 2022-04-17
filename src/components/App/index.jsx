76aca615d5d6a85a3232632b8736150fc8a1a83a
Tom McAndrew <tom.mcandrew@appscore.net>
2022-04-17 23:02:35 +0100

    install react-masonry-css and use for main layout of cards

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import Masonry from "react-masonry-css";
 import fashionImages from "../../assets/images/images";
 import ManualCard from "../ManualCard";
 import InstagramCard from "../InstagramCard";
@@ -7,12 +8,19 @@ import TwitterCard from "../TwitterCard";
 import GlobalStyle from "../../styles/global";
 import { AppWrapper } from "../../styles/app";
 
+const breakpointColumnsObj = {
+  default: 4,
+  1100: 3,
+  700: 2,
+  500: 1,
+};
+
 function App() {
   const [posts, setPosts] = useState([]);
 
   async function fetchData() {
     const res = await axios.get(
-      "http://private-cc77e-aff.apiary-mock.com/posts",
+      "http://private-cc77e-aff.apiary-mock.com/posts"
     );
     setPosts(res.data.items);
   }
@@ -31,30 +39,36 @@ function App() {
     <>
       <GlobalStyle />
       <AppWrapper>
-        {posts.map((post, i) => {
-          if (post.service_name === "Manual") {
-            return (
-              <div key={post.item_id}>
-                <ManualCard post={post} img={fashionImages[i]} />
-              </div>
-            );
-          }
-          if (post.service_name === "Instagram") {
-            return (
-              <div key={post.item_id}>
-                <InstagramCard post={post} img={getRandomImageUrl()} />
-              </div>
-            );
-          }
-          if (post.service_name === "Twitter") {
-            return (
-              <div key={post.item_id}>
-                <TwitterCard post={post}/>
-              </div>
-            );
-          }
-          return null;
-        })}
+        <Masonry
+          breakpointCols={breakpointColumnsObj}
+          className="my-masonry-grid"
+          columnClassName="my-masonry-grid_column"
+        >
+          {posts.map((post, i) => {
+            if (post.service_name === "Manual") {
+              return (
+                <div key={post.item_id}>
+                  <ManualCard post={post} img={fashionImages[i]} />
+                </div>
+              );
+            }
+            if (post.service_name === "Instagram") {
+              return (
+                <div key={post.item_id}>
+                  <InstagramCard post={post} img={getRandomImageUrl()} />
+                </div>
+              );
+            }
+            if (post.service_name === "Twitter") {
+              return (
+                <div key={post.item_id}>
+                  <TwitterCard post={post} />
+                </div>
+              );
+            }
+            return null;
+          })}
+        </Masonry>
       </AppWrapper>
     </>
   );
--- src/styles/app.js
+++ src/styles/app.js
@@ -1,5 +1,17 @@
+/* eslint-disable import/prefer-default-export */
 import styled from "styled-components";
 
 export const AppWrapper = styled.div`
-    padding: 2rem 0;
-`
+  padding: 2rem 0;
+  .my-masonry-grid {
+    display: -webkit-box;
+    display: -ms-flexbox;
+    display: flex;
+    margin-left: -30px;
+    width: auto;
+  }
+  .my-masonry-grid_column {
+    padding-left: 30px;
+    background-clip: padding-box;
+  }
+`;
