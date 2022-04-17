62f06b036bcc70bdd05352ba4f49368f92d78730
Tom McAndrew <tom.mcandrew@appscore.net>
2022-04-17 21:09:40 +0100

    add some styles for Card components

---
+++ src/styles/card.js
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+
+export const CardWrapper = styled.div`
+    margin: 0 1rem 5rem 1rem;
+    border: 1px solid rgba(0,0,0,0.2);
+    border-radius: 6px;
+    padding: 0.5rem;
+`;
+
+export const CardText = styled.p`
+margin: 1rem 0;
+`;
+
+export const CardImageWrapper = styled.div`
+    width: 100%;
+    `;
+
+export const CardLinkWrapper = styled.div`
+    display: flex;
+    justify-content: center;
+    `;
+
+export const CardContent = styled.div`
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    padding: 1rem;
+`;
