849fb65032147c486482bff3e11dc7ea26cf8c20
Tom McAndrew <tom.mcandrew@appscore.net>
2022-04-17 22:02:11 +0100

    render image, text and link in ManualCard

--- src/components/ManualCard/index.jsx
+++ src/components/ManualCard/index.jsx
@@ -1,9 +1,28 @@
 import React from "react";
+import PropTypes from "prop-types";
+import {
+  CardWrapper, CardImageWrapper, CardText, CardLinkWrapper, CardContent,
+} from "../../styles/card";
 
-function ManualCard() {
+function ManualCard({ post, img }) {
   return (
-    <div></div>
+    <CardWrapper>
+      <CardContent>
+        <CardImageWrapper>
+          <img src={img} alt="#" width="100%" />
+        </CardImageWrapper>
+        <CardText>{post.item_data.text}</CardText>
+        <CardLinkWrapper>
+          <a href={post.item_data.link} target="_blank" rel="noreferrer">{post.item_data.link_text}</a>
+        </CardLinkWrapper>
+      </CardContent>
+    </CardWrapper>
   );
 }
+
+ManualCard.propTypes = {
+  post: PropTypes.object,
+  img: PropTypes.string,
+};
 
 export default ManualCard;
