6df2924306795d2ccd6dc5beb0c3c717c1556a6b
Tom McAndrew <tom.mcandrew@appscore.net>
2022-04-18 01:09:07 +0100

    add basic button that loads the same 20 posts again and adds them to the list, create getImageUrl function that assigns the same image to the same post after loading more

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -4,7 +4,7 @@ import Masonry from "react-masonry-css";
 import Card from "../Card";
 import GlobalStyle from "../../styles/global";
 import { AppWrapper } from "../../styles/app";
-import { getRandomImageUrl } from "../../utils/imageUtils";
+import fashionImages from "../../assets/images/images";
 import { sortItemsByDate } from "../../utils/dateUtils";
 import sortOptions from "../../enums/sortOptions";
 
@@ -17,16 +17,33 @@ const breakpointColumnsObj = {
 function App() {
   const [posts, setPosts] = useState([]);
 
-  async function fetchData() {
+  async function getData() {
     const res = await axios.get(
       "http://private-cc77e-aff.apiary-mock.com/posts",
     );
-    setPosts(sortItemsByDate(res.data.items, sortOptions.ASC));
+    return res.data.items;
   }
 
   useEffect(() => {
-    fetchData();
+    async function updatePosts() {
+      const items = await getData();
+      const sortedPosts = sortItemsByDate(items, sortOptions.ASC);
+      setPosts(sortedPosts);
+    }
+    updatePosts();
   }, []);
+
+  const handleLoadMore = async () => {
+    const morePosts = await getData();
+    const sortedPosts = sortItemsByDate(morePosts, sortOptions.ASC);
+    setPosts([...posts, ...sortedPosts]);
+  };
+
+  const getImageUrl = (index) => {
+    const pageIndex = Math.floor(index / 20);
+    const imageIndex = index - (20 * pageIndex);
+    return fashionImages[imageIndex];
+  };
 
   return (
     <>
@@ -37,8 +54,16 @@ function App() {
           className="my-masonry-grid"
           columnClassName="my-masonry-grid_column"
         >
-          {posts.map((post) => <Card key={post.item_id} post={post} img={getRandomImageUrl()} />)}
+          {posts
+            .map((post, i) => (
+              <Card
+                key={post.item_id}
+                post={post}
+                img={getImageUrl(i)}
+              />
+            ))}
         </Masonry>
+        <button onClick={handleLoadMore} type="button">More</button>
       </AppWrapper>
     </>
   );
